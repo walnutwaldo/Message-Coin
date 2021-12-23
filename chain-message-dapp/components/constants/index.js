28f691387bb9b1b7fe6848f7d197388553bc49f2
Walden Yan <waldenyan20@gmail.com>
2021-12-23 00:52:13 -0500

    moved contract address into env file

--- chain-message-dapp/components/constants/index.js
+++ chain-message-dapp/components/constants/index.js
@@ -1,4 +1,4 @@
 import MessageCoinContract from '../contracts/MessageCoin.json';
 
-export const MESSAGE_COIN_CONTRACT_ADDRESS = "0xDd3b6fD2C80fCF7352C5B181c620cD773B9f3928"; // "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const MESSAGE_COIN_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS; // "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const MESSAGE_COIN_ABI = MessageCoinContract.abi;
